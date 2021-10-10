9dadda47a11549c6c48dbac0670596ac711b0a10
raulale@student.ubc.ca <raulale@student.ubc.ca>
2021-10-10 16:47:52 -0700

    Tentative

--- Exercise3/Form1.Designer.cs
+++ Exercise3/Form1.Designer.cs
@@ -30,36 +30,45 @@ namespace Exercise3
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timerRNG = new System.Windows.Forms.Timer(this.components);
             this.button1 = new System.Windows.Forms.Button();
             this.textBoxRand = new System.Windows.Forms.TextBox();
+            this.timerHalf = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
             this.SuspendLayout();
             // 
-            // timer1
+            // timerRNG
             // 
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            this.timerRNG.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(328, 193);
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(298, 178);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(124, 53);
+            this.button1.Size = new System.Drawing.Size(203, 53);
             this.button1.TabIndex = 0;
             this.button1.Text = "New Time";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBoxRand
             // 
-            this.textBoxRand.Location = new System.Drawing.Point(328, 265);
+            this.textBoxRand.Location = new System.Drawing.Point(298, 237);
             this.textBoxRand.Name = "textBoxRand";
-            this.textBoxRand.Size = new System.Drawing.Size(124, 31);
+            this.textBoxRand.Size = new System.Drawing.Size(203, 31);
             this.textBoxRand.TabIndex = 1;
+            // 
+            // timerHalf
+            // 
+            this.timerHalf.Interval = 500;
+            this.timerHalf.Tick += new System.EventHandler(this.timerHalf_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.textBoxRand);
             this.Controls.Add(this.button1);
@@ -72,9 +81,11 @@ namespace Exercise3
 
         #endregion
 
-        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timerRNG;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBoxRand;
+        private System.Windows.Forms.Timer timerHalf;
+        private System.IO.Ports.SerialPort serialPort1;
     }
 }
 
--- Exercise3/Form1.cs
+++ Exercise3/Form1.cs
@@ -12,6 +12,8 @@ namespace Exercise3
 {
     public partial class Form1 : Form
     {
+        int wait = 0;
+        
         public Form1()
         {
             InitializeComponent();
@@ -20,14 +22,29 @@ namespace Exercise3
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-
+            var rand = new Random();
+            serialPort1.Write("5");
+            int r = rand.Next(255);
+            int g = rand.Next(255);
+            int b = rand.Next(255);
+            button1.BackColor = Color.FromArgb(r,g,b);
+            timerHalf.Enabled = true;
+            timerRNG.Enabled = false;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             var rand = new Random();
-            int num = rand.Next(1000);
+            int num = rand.Next(200,1000);
             textBoxRand.Text = num.ToString();
+            timerRNG.Interval = num;
+            timerRNG.Enabled = true;
+        }
+
+        private void timerHalf_Tick(object sender, EventArgs e)
+        {
+            serialPort1.Write("%");
+            timerHalf.Enabled = false;
         }
     }
 }
